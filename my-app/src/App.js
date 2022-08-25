07da5738ae5dc6e5c4908d508e3a6e46b176a73b
wayangalihpratama <wgprtm@gmail.com>
2022-08-25 18:01:30 +0800

    First test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          data-testid="link-button"
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
